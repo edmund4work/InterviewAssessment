a4eb6a89785a3541047d308b0eec7b4d68d31489
edmund4work <edmund4work@users.noreply.github.com>
2024-09-07 17:48:11 +0800

    1st interview question

---
+++ DotNet/20240916RushDigital/20240916RushDigital/Controllers/fibonacciController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _20240916RushDigital.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class fibonacciController : ControllerBase
+    {
+        [HttpGet(Name = "getFibonacci")]
+        public returnFieldsFibonacci CalculateFibonacciRecursive(int n)
+        {
+            returnFieldsFibonacci returnData = new returnFieldsFibonacci();
+            returnData.listOfFibonacci = new List<int>();
+
+            //https://en.wikipedia.org/wiki/Fibonacci_sequence
+            if (n <= 1)
+            {
+                returnData.returnValue = "0";// "Enter value must more than 1";
+            }
+            else
+            {
+                int number = n - 1; //Need to decrement by 1 since we are starting from 0
+                int[] Fib = new int[number + 1];
+                Fib[0] = 0;
+                Fib[1] = 1;
+                for (int i = 2; i <= number; i++)
+                {
+                    Fib[i] = Fib[i - 2] + Fib[i - 1];
+                    if (Fib[i] > n)
+                        break;
+                    else if (Fib[i] < n)
+                        returnData.returnValue = "nothing"; //break;
+                    else
+                        returnData.returnValue = Fib[i - 1].ToString();
+
+                    returnData.listOfFibonacci.Add(Fib[i]);
+                }
+            }
+
+
+            return returnData;
+        }
+
+        public class returnFieldsFibonacci
+        {
+            public string returnValue { get; set; }
+            public List<int> listOfFibonacci { get; set; }
+        }
+    }
+}
+
---
+++ DotNet/20240916RushDigitalFirst/20240916RushDigitalFirst/EmptyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace _20240916RushDigitalFirst
+{
+	public class EmptyClass
+	{
+		public static int Calculate(int n)
+		{
+            //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+
+            if (n < 2)
+                return n;
+            else
+                return Calculate(n - 1) + Calculate(n - 2);
+        }
+    }
+}
+
